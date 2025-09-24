46d5da4169a89f6a764bc96a7d59c0f28506e5ff
ivoalterman <ivoaltermann@gmail.com>
2025-09-24 10:40:20 -0300

    Damos por comenzado el proyecto con la primer version de html, css y js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Página cargada correctamente 🚀");
+
+  // Aquí después programamos el carrito
+  const carrito = document.querySelector(".carrito");
+  carrito.addEventListener("click", () => {
+    alert("Carrito desplegable próximamente...");
+  });
+});
