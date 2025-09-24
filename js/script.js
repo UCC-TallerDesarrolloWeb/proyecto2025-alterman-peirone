804bd03de60974e13bbd54b58530cc9b2d137b9c
ivoalterman <ivoaltermann@gmail.com>
2025-09-24 13:01:13 -0300

    suerte

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Página cargada 🚀");
-
   const menuBtn = document.getElementById("menu-btn");
   const menu = document.getElementById("menu");
 
   menuBtn.addEventListener("click", () => {
-    // Toggle: si está oculto lo muestra, si está visible lo oculta
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   });
 });
+
